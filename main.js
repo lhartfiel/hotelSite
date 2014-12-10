75745bd484fbd5ce8cde13616674aa8be4cdda17
lhartfiel <lkhartfiel@uwalumni.com>
2014-12-10 15:16:19 -0600

    Contact completed and responsiveness

--- main.js
+++ main.js
@@ -1,16 +1,103 @@
-var homeHover = function(){
-	$('.homeImg').on('mouseleave', function(){
-		$('.overlayBody').fadeOut();
-		$(this).removeClass('imgOverlay');
-		
+
+// homepage hero image slider
+// var homeHero = function(){		
+// 	var homeClasses = ['heading', 'heading2', 'heading3', 'heading4'];
+// 	i = 0
+
+// 	$.each(homeClasses, function(i, value){
+// 		$('.headInitial').removeClass(homeClasses).addClass(homeClasses[i]).css({'opacity': 1}).show().delay(1000).fadeOut(1000);
+// 			if(i == homeClasses.length - 1){
+// 				i = 0;
+// 			} else {
+// 				i++;
+// 			}
+// 			console.log(value)
+// 	});
+// };
+
+var homeHero = function(){
+	var homeClasses = ['heading', 'heading2', 'heading3', 'heading4'];
+	counter = 0;
+	setInterval(function(){
+		$('.headInitial').removeClass('heading heading2 heading3 heading4').fadeIn(4000).addClass(homeClasses[counter]).show().css({'transition': '4s'}).delay(9000);
+		if(counter === homeClasses.length - 1){
+			counter = 0;
+		}else{
+			counter++;
+		};
+	}, 7000);
+	console.log(counter);
+};	
+
+// var homeHero = function(){
+// 	var homeClasses = ['heading', 'heading2', 'heading3', 'heading4'];
+// 	var i;
+	
+// 		var className = function(){$.each(homeClasses, function(i){
+// 			var currentName = $(this).fadeIn(1000).delay(5000).fadeOut(1000);	
+// 			if (i === 0){
+// 				currentName = ($(this).get().join(""));
+// 			}else if (i === 1){
+// 				currentName = ($(this).get().join(""));
+// 			}else if (i === 2){
+// 				currentName = ($(this).get().join(""));
+// 			}else {
+// 				currentName = ($(this).get().join(""));
+// 			};
+// 			console.log(currentName);
+// 			$('.headInitial').removeClass(currentName);
+// 			$('.headInitial').addClass(currentName).animate({'opacity': 1}).delay(5000).stop();
+// 				console.log(currentName)	
+// 				});
+				
+// 		};
+// 		className();
+// 	setInterval(function(){className}, 1000);
+// };	
+
+
+	
+
+var logoChange = function(){
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 500) {
+			$('.logo').hide();
+			$('.miniLogo').show();
+			$('.imgLogo').css({'top':'15px', 'left':'0px'});
+			$('.intro').animate({'opacity':1}, 1200);	
+		} else {
+			$('.logo').show();
+			$('.miniLogo').hide();
+		}; 
 	});
-	$('.homeImg').on('mouseenter', function(){
-		$(this).addClass('imgOverlay').fadeIn().find('p').addClass('overlayBody').fadeIn(800);
-		});
 };
 
-var roomSlide = function(){
+var homeSlide = function(){
+	$('a[href=#topNav]').on('click', function(e){
+		e.preventDefault();
+		$('html,body').animate({
+			scrollTop:$(this.hash).offset().top
+		}, 800);
+		console.log('success scroll')
+	});
+};
 
+// Slidedown effect for homepage images on homepage
+var homeHover = function(){
+	$('.homeImg').each(function(){
+		$(this).on('mouseleave', function(){
+			$(this).find('p').slideUp(1200);
+		})
+	})
+	$('.homeImg').each(function(){
+		$(this).on('mouseenter', function(){
+			$(this).find('p').slideDown(1200)
+		});
+	});
+};
+
+// Accordion effect for Room types
+var roomSlide = function(){
 	$('.rooms').each(function(){
 		$(this).on('click', '.plus', function(){
 			$(this).parent().parent().parent().addClass('ImgSection').next().slideDown(1500);
@@ -25,15 +112,34 @@ var roomSlide = function(){
 	});
 };
 
+// Date picker on Rooms page sidebar
 var pickDate = function(){
-    $( "#datepicker" ).datepicker();
+    $("#datepicker").datepicker();
     $("#datepicker2").datepicker();
-}
+};
+
+
+// Navigation hamburger bar
+var hamburgerNav = function(){
+	// $('nav .fa').on('click', function(){
+	// 	$('nav ul').slideUp(1000).stop();
+	// });
+	$('nav .fa').on('click', function(){
+		$('nav ul').slideToggle('slow', function(){
+			$('nav ul li ul').show();	
+		})
+	});
+};
+
 
 $(document).ready(function(){
 	$('.overlay').fadeIn(2100);
+	logoChange();
 	homeHover();
 	roomSlide();
-	pickDate();
+	pickDate();	
+	homeSlide();
+	homeHero();
+	hamburgerNav();
 });
 	
